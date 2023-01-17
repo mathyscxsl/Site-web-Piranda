7c2a010e3e5c48689452e403f638c3e0e44cca06
MaathBluee <mathys.cunsolo@gmail.com>
2023-01-17 12:53:35 +0100

    Retrait du JS, page artisan
    J'ai retiré le JS car il y'avait une manière plus pratique de faire ce que je voulais en mettant le header en position sticky, j'ai fait le contenu et le back de la page artisan

--- templates/page-artisan.php
+++ templates/page-artisan.php
@@ -5,9 +5,32 @@
 
     // Header
     get_header();
+
+    // On récupère les informations relatives aux images de chaque section de la page index (image, alt..)
+    $image_profil = get_field('image_profil');
+
+    // Foreach images de la galerie
+    $images_profil = get_field('galerie_profil');
 ?>
 
-PAGE ARTISAN
+    <section class="profil">
+        <img src="<?php echo($image_profil['url']);?>" width="<?php echo($image_profil['width']);?>" height="<?php echo($image_profil['height']);?>" alt="<?php echo($image_profil['alt']);?>">
+        <div class="profil_texte">
+            <h1><?php echo(the_field('titre_profil'));?></h1>
+            <p><?php echo(the_field('desc_profil'));?></p>
+        </div>
+    </section>
+
+    <section class="galerie_profil">
+        <p><?php echo(the_field('paragraphe_profil'));?></p>
+        <div class="galerie_profil_images">
+            <?php foreach($images_profil as $image_profil): ?>
+            <article>
+                <img src="<?php echo($image_profil['url']);?>" width="<?php echo($image_profil['width']);?>" height="<?php echo($image_profil['height']);?>" alt="<?php echo($image_profil['alt']);?>">
+            </article>
+            <?php endforeach; ?>
+        </div>
+    </section>
 
 <?php
     // Footer
