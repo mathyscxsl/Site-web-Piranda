673cba715f5aace2bb57e19469f04dbcc2e16f5b
MaathBluee <mathys.cunsolo@gmail.com>
2023-01-17 11:30:07 +0100

    Préchargement des dimensions des images
    J'ai ajouté l'attribut width & height à chaque image de manière à précharger l'espace qui leur est attribué.

--- templates/page-accueil.php
+++ templates/page-accueil.php
@@ -26,7 +26,7 @@
     </div>
 
     <section class="entreprise">
-        <img src="<?php echo($image_entreprise['url']);?>" alt="<?php echo($image_entreprise['alt']);?>">
+        <img src="<?php echo($image_entreprise['url']);?>" width="<?php echo($image_entreprise['width']);?>" height="<?php echo($image_entreprise['height']);?>" alt="<?php echo($image_entreprise['alt']);?>">
         <div class="entreprise-texte">
             <h2><?php echo(the_field('titre_principal_entreprise'));?></h2>
             <h3><?php echo(the_field('titre_paragraphe_entreprise'));?></h3>
@@ -37,7 +37,7 @@
     </section>
 
     <section class="travail">
-        <img src="<?php echo($image_travail['url']);?>" alt="<?php echo($image_travail['alt']);?>">
+        <img src="<?php echo($image_travail['url']);?>" width="<?php echo($image_travail['width']);?>" height="<?php echo($image_travail['height']);?>" alt="<?php echo($image_travail['alt']);?>">
         <div class="travail-texte">
             <h2><?php echo(the_field('titre_principal_travail'));?></h2>
             <h3><?php echo(the_field('titre_paragraphe_travail'));?></h3>
@@ -50,7 +50,7 @@
     <section class="qualites">
         <div class="fait_main">
             <div class="fait_main_image">
-                <img src="<?php echo($image_fait_main['url']);?>" alt="<?php echo($image_fait_main['alt']);?>">
+                <img src="<?php echo($image_fait_main['url']);?>" width="<?php echo($image_fait_main['width']);?>" height="<?php echo($image_fait_main['height']);?>" alt="<?php echo($image_fait_main['alt']);?>">
             </div>
             <div class="fait_main_texte">
                 <h2><?php echo(the_field('titre_principal_fait_main'));?></h2>
@@ -61,7 +61,7 @@
         </div>
         <div class="sur_mesure">
             <div class="sur_mesure_image">
-                <img src="<?php echo($image_sur_mesure['url']);?>" alt="<?php echo($image_sur_mesure['alt']);?>">
+                <img src="<?php echo($image_sur_mesure['url']);?>" width="<?php echo($image_sur_mesure['width']);?>" height="<?php echo($image_sur_mesure['height']);?>" alt="<?php echo($image_sur_mesure['alt']);?>">
             </div>
             <div class="sur_mesure_texte">
                 <h2><?php echo(the_field('titre_principal_sur_mesure'));?></h2>
@@ -72,14 +72,14 @@
         </div>
         <div class="qualites_atouts">
             <div class="qualites_atout">
-                <img src="<?php echo($image_atout1['url']);?>" alt="<?php echo($image_atout1['alt']);?>">
+                <img src="<?php echo($image_atout1['url']);?>" width="<?php echo($image_atout1['width']);?>" height="<?php echo($image_atout1['height']);?>" alt="<?php echo($image_atout1['alt']);?>">
                 <div class="qualites_texte">
                     <p><?php echo(the_field('titre_atout1'));?></p>
                     <p><?php echo(the_field('desc_atout1'));?></p>
                 </div>
             </div>
             <div class="qualites_atout">
-                <img src="<?php echo($image_atout2['url']);?>" alt="<?php echo($image_atout2['alt']);?>">
+                <img src="<?php echo($image_atout2['url']);?>" width="<?php echo($image_atout2['width']);?>" height="<?php echo($image_atout2['height']);?>" alt="<?php echo($image_atout2['alt']);?>">
                 <div class="qualites_texte">
                     <p><?php echo(the_field('titre_atout2'));?></p>
                     <p><?php echo(the_field('desc_atout2'));?></p>
@@ -90,7 +90,7 @@
 
     <section class="galerie">
         <div class="galerie_top">
-            <img src="<?php echo($image_galerie_top['url']);?>" alt="<?php echo($image_galerie_top['alt']);?>">
+            <img src="<?php echo($image_galerie_top['url']);?>" width="<?php echo($image_galerie_top['width']);?>" height="<?php echo($image_galerie_top['height']);?>" alt="<?php echo($image_galerie_top['alt']);?>">
         </div>
         <div class="galerie_content">
             <h3><?php echo(the_field('titre_galerie'));?></h3>
